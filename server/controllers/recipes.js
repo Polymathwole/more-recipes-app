591d90e5b019cee066ef6a937dd51904a7ef5197
Oluwaremi Oluwole <polymathwole@gmail.com>
2017-09-28 12:04:46 +0100

    Add dummy data

--- server/controllers/recipes.js
+++ server/controllers/recipes.js
@@ -1,6 +1,8 @@
 const Recipe=require('../models').Recipe;
 const Review=require('../models').Review;
 
+const recipes = [];
+
 module.exports = {
     create:(req, res)=>{
       return Recipe
@@ -13,6 +15,23 @@ module.exports = {
         })
         .then(recipe => res.status(201).json(recipe))
         .catch(error =>res.status(400).json({detail:error.parent.detail}))
+  },
+  createDummy:(req,res)=>{
+    data={
+      title: req.body.title,
+      content: req.body.content,
+      upvotes:0,
+      downvotes:0,
+      creatorId: req.body.creatorId
+    };
+
+    if (recipes.length===0)
+      data.id=1
+    else
+      data.id=recipes[recipes.length-1].id+1
+
+    recipes.push(data);
+    res.status(201).json(data)
   },
 
   list:(req, res)=>{
@@ -39,6 +58,9 @@ module.exports = {
       .then(recipes => res.status(200).json(recipes))
       .catch(error => res.status(400).json({error}));
   },
+  listDummy:(req,res)=>{
+    res.status(201).json(recipes);
+  },
 
   update:(req,res)=>{
     return Recipe
@@ -53,6 +75,32 @@ module.exports = {
           .catch((error) => res.status(400).json({detail:error.parent.detail}));
         })
         .catch((error) => res.status(400).json({detail:error.parent.detail}));
+  },
+
+  updateDummy:(req,res)=>{
+      let recipeId=undefined;
+      for (let i=0;i<recipes.length;++i)
+      {
+        if (req.params.recipeId===recipes[i].id)
+          {
+            let recipe=recipes[i]
+            
+            for (let k of Object.keys(req.body))
+            {
+              recipes[id][k]=req.body[k];
+            }
+
+            return res.status(200).json(recipe[i])
+          }
+
+          if (i===recipes[recipes.length-1]&&(req.params.recipeId!==recipes[i].id))
+            return res.status(400).json({message:'Recipe Not Found'});
+      }
+
+      if (recipeId)
+      {
+
+      }
   },
 
   delete:(req,res)=>{
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,16 @@ const recipeController = require('../controllers').recipes;
 const reviewController = require('../controllers').reviews;
 
 module.exports = (app) => {
-    app.get('/api/v1/recipes?sort=upvotes&order=des', recipeController.listByUpvotes);
+    app.post('/api/v1/recipes', recipeController.createDummy);
+    app.get('/api/v1/recipes', recipeController.listDummy);
+    app.put('/api/v1/recipes/:recipeId', recipeController.updateDummy);
+
+    /*
     app.post('/api/v1/recipes', recipeController.create);
     app.get('/api/v1/recipes', recipeController.list);
     app.put('/api/v1/recipes/:recipeId', recipeController.update);
     app.delete('/api/v1/recipes/:recipeId', recipeController.delete);
     app.post('/api/v1/recipes/:recipeId/reviews', reviewController.create);
+    app.get('/api/v1/recipes?sort=upvotes&order=des', recipeController.listByUpvotes);
+    */
 }
