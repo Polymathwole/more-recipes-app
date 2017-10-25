d29a99b912c7598ba416fcf910bfb0dc41b43f2e
Oluwaremi Oluwole <polymathwole@gmail.com>
2017-10-25 17:15:41 +0100

    Update Express to version 4.16.2

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import logger from 'morgan';
-import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import routes from './routes';
 
@@ -13,8 +12,8 @@ app.disable('x-powered-by');
 
 app.use(logger('dev'));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 routes(app);
 
 app.get('/', (req, res) => res.status(200).json({
